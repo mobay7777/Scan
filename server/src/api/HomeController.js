d0f27bca7429e3d23fe42500b31b3d05cb878fe7
rupayaproject <mstf.baig@gmail.com>
2020-03-17 19:18:38 +0000

    add totalcoins

--- server/src/api/HomeController.js
+++ server/src/api/HomeController.js
@@ -23,6 +23,21 @@ HomeController.get('/circulatingSupply', async (req, res) => {
     let maxSupply = 100 * 10 ** 6
     return res.json({ circulatingSupply: circulatingNumber.toNumber(), maxSupply: maxSupply })
 })
+HomeController.get('/totalcoins', async (req, res) => {
+    const web3 = await Web3Util.getWeb3()
+    let foundationBalance = await web3.eth.getBalance(contractAddress.RupxFoundation)
+    let teamBalance = await web3.eth.getBalance(contractAddress.RupxTeam)
+    let lastBlock = await web3.eth.getBlockNumber()
+    let totalEpoch = Math.ceil(lastBlock / config.get('BLOCK_PER_EPOCH'))
+    let totalReward = new BigNumber((totalEpoch - 1) * config.get('REWARD'))
+    let circulatingSupply = new BigNumber(72 * 10 ** 6)
+    circulatingSupply = circulatingSupply.multipliedBy(10 ** 18)
+        .plus(totalReward.multipliedBy(10 ** 18)).minus(foundationBalance).minus(teamBalance)
+
+    let circulatingNumber = circulatingSupply.dividedBy(10 ** 18)
+
+    return res.json(circulatingNumber.toNumber())
+})
 
 HomeController.get('/jobNumber', async (req, res) => {
     let countJobs = () => {
